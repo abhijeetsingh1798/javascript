00bd39fe687b16638b65982884a6fe2d0b3b3d63
abhijeetsingh1798 <abhijeet.rajput1798@gmail.com>
2023-06-09 08:00:53 +0530

    change

---
+++ 05_operatorAssig.js
@@ -0,0 +1,25 @@
+
+
+
+var studentName = "Sonu Sharma";
+var sonuSsc = 90;
+var sonuHsc = 80;
+var sonuGraduation = 70;
+var result = sonuSsc>=90 && sonuHsc>=80 && sonuGraduation>=70 ? "Congrates! Sonu You are eligible for TCS interview" : " unfortunately You are not eligible for TCS interview";
+console.log(`${result}`);
+console.log("--------------------------------------------------------------------------------------------------------");
+
+var studentName = "Rahul";
+var rahulSsc = 80;
+var rahulHsc = 86;
+var rahulGraduation = 90;
+var result = rahulSsc>=90 && rahulHsc>=80 && rahulGraduation>=70 ? "Congraates! Rahul You are allow for TCS interview" : " You are not eligible for TCS interview";
+console.log(`${result}`);
+console.log("--------------------------------------------------------------------------------------------------------");
+var studentName = "shambhu";
+var shambhuSsc = 60;
+var shambhuHsc = 79;
+var shambhuGraduation = 88;
+var result = shambhuSsc>=90 && shambhuHsc>=80 && shambhuGraduation>=70 ? "Congraates! Shambhu You are allow for TCS interview" : " You are not eligible for TCS interview";
+console.log(`${result}`);
+console.log("--------------------------------------------------------------------------------------------------------");
